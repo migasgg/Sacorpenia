dfc962e551990510496156c23a3b866d1ff5f6f6
Miguel Albert <miguelalbert38@gmail.com>
2024-04-17 22:31:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const cName = document.querySelector('#name')
+const mc = document.querySelector('#mc')
+
+
+function massaMuscularEsqueletica(event) {
+  event.preventDefault()
+
+  const mc = document.querySelector('#mc')
+  console.log(mc)
+}
